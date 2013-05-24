bf81d7ec69d0207d954f2942ca798048bac90868
Rémi Prévost <remi@exomel.com>
2013-05-24 15:19:22 -0400

    Fix bugs

--- lib/active_model/serializer/validator/mixin.rb
+++ lib/active_model/serializer/validator/mixin.rb
@@ -18,7 +18,7 @@ module ActiveModel
           # Set the JSON schema to use for this class
           def json_schema(value = nil)
             @_json_schema ||= begin
-              superclass.primary_key if superclass.respond_to?(:json_schema)
+              superclass.json_schema if superclass.respond_to?(:json_schema)
             end
 
             return @_json_schema unless value
--- lib/active_model_serializers_validator.rb
+++ lib/active_model_serializers_validator.rb
@@ -1,5 +1,6 @@
 require 'json-schema'
-require 'active_model/serializer/validator'
+require 'active_model/serializer/validator/mixin'
+require 'active_model/serializer/validator/version'
 
 # Inject the mixin in the ActiveModel::Serializer class
 ActiveModel::Serializer.send :include, ActiveModel::Serializer::Validator::Mixin
